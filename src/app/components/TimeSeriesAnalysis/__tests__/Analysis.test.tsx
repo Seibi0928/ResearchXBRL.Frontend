1d37e8971ac64757a1170541bc1d87cd3fa97315
e a <nanteneus@hotmail.co.jp>
2021-12-06 16:17:52 +0000

    テストコード追加

---
+++ src/app/components/TimeSeriesAnalysis/__tests__/Analysis.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { TimeSeriesAnalysis } from '../Analysis';
+import { TimeSeriesAnalysisRepository } from '../Menu';
+
+describe('AnalysisTest', () => {
+  test('二つの検索ボックスがある', () => {
+    const repositoryMock = createMockRepository();
+    render(<TimeSeriesAnalysis repository={repositoryMock()} />);
+
+    expect(screen.getAllByRole('search').length).toEqual(2);
+  });
+});
+
+function createMockRepository() {
+  return jest.fn<TimeSeriesAnalysisRepository, []>()
+    .mockImplementation(() => {
+      return {
+        getAccountItems: async (_: string) => {
+          return new Promise(r => r([{ label: '売掛金', value: '売掛金' }]));
+        },
+        getCorporations: (_: string) => {
+          return new Promise(r => r([{ label: '鮭産業', value: 'Exxxx' }]));
+        },
+        getAnalysisResult: (_: string, __: string) => {
+          return new Promise(r => r({
+            accountName: '',
+            unit: {},
+            corporation: { name: '' },
+            values: [{ financialAccountPeriod: { instant: '2021-02-01' }, amount: 111 }]
+          }));
+        }
+      };
+    });
+}
