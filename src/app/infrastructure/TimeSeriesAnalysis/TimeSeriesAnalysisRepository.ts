f596ad2496dbdbfc92ce26c86f395a66aaa690ce
Seibi0928 <nanteneus@hotmail.co.jp>
2021-12-06 17:28:10 +0000

    フロントエンドからAPIへ疎通できるようにする

--- src/app/infrastructure/TimeSeriesAnalysis/TimeSeriesAnalysisRepository.ts
+++ src/app/infrastructure/TimeSeriesAnalysis/TimeSeriesAnalysisRepository.ts
@@ -12,7 +12,7 @@ type AccountItemInfo = {
 export class TimeSeriesAnalysisRepositoryImpl implements TimeSeriesAnalysisRepository {
   async getCorporations(keyword: string): Promise<CorporationOption[]> {
     // TOOD: urlを変更
-    const response = await fetch(`http://localhost:8080/AnalysisMenu/suggest/corporations?keyword=${keyword}`);
+    const response = await fetch(`api/AnalysisMenu/suggest/corporations?keyword=${keyword}`);
     const responsBody = await response.json() as CorporationInfo[];
     return responsBody.map(c => ({
       value: c.corporationId,
@@ -22,7 +22,7 @@ export class TimeSeriesAnalysisRepositoryImpl implements TimeSeriesAnalysisRepos
 
   async getAccountItems(keyword: string): Promise<AccountItemOption[]> {
     // TOOD: urlを変更
-    const response = await fetch(`http://localhost:8080/AnalysisMenu/suggest/accountItems?keyword=${keyword}`);
+    const response = await fetch(`api/AnalysisMenu/suggest/accountItems?keyword=${keyword}`);
     const responsBody = await response.json() as AccountItemInfo[];
     return responsBody.map(c => ({
       value: c.name,
@@ -31,7 +31,7 @@ export class TimeSeriesAnalysisRepositoryImpl implements TimeSeriesAnalysisRepos
   }
 
   async getAnalysisResult(corporationId: string, accountItemName: string): Promise<TimeSeriesAnalysisResposeValue> {
-    const response = await fetch(`http://localhost:8080/TimeSeriesAnalysis/result?corporationId=${corporationId}&accountItemName=${accountItemName}`);
+    const response = await fetch(`api/TimeSeriesAnalysis/result?corporationId=${corporationId}&accountItemName=${accountItemName}`);
     return response.json();
   }
 }
