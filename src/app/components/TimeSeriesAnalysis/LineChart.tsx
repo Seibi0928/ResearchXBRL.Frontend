85bba22322d5a09a7101188e91b5725d997860aa
Seibi0928 <nanteneus@hotmail.co.jp>
2021-12-12 05:45:34 +0000

    分析画面タイトル改善 グラフに縦軸単位付きのタイトルを表示 日付及び数値が会計期間順にならんでいないバグの修正 会計期間表示

--- src/app/components/TimeSeriesAnalysis/LineChart.tsx
+++ src/app/components/TimeSeriesAnalysis/LineChart.tsx
@@ -9,6 +9,7 @@ import {
   Legend,
   ChartData
 } from 'chart.js'
+import dayjs from 'dayjs';
 import React from "react";
 import { Line } from "react-chartjs-2";
 import { AccountItemOption, CorporationOption, TimeSeriesAnalysisRepository } from "./Menu";
@@ -33,37 +34,49 @@ export const performTimeSeriesAnalysis = async (
 const convertToChartData = (response: TimeSeriesAnalysisResposeValue)
   : ChartData<"line", number[], string> => {
   const toLabel = (value: TimeSeriesAnalysisValue): string => {
-    if (isInstansPeriod(value.financialAccountPeriod)) {
-      return value.financialAccountPeriod.instant;
+    if (isInstantPeriod(value.financialAccountPeriod)) {
+      return dayjs(value.financialAccountPeriod.instant).format('YYYY/MM');
     }
-    return value.financialAccountPeriod.from;
+    return dayjs(value.financialAccountPeriod.from).format('YYYY/MM');
   };
 
   const labelsSet = new Set(response.consolidatedValues
     .map(toLabel).concat(response.nonConsolidatedValues
-    .map(toLabel)));
+      .map(toLabel)));
   return {
-    labels: [...labelsSet],
+    labels: [...labelsSet].sort(),
     datasets: [{
       label: `連結財務諸表`,
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
-      data: response.consolidatedValues.map(v => v.amount)
+      data: response.consolidatedValues.sort(comparePeriod).map(v => v.amount)
     },
     {
       label: `単体財務諸表`,
       backgroundColor: 'rgb(77, 196, 255)',
       borderColor: 'rgb(77, 196, 255)',
-      data: response.nonConsolidatedValues.map(v => v.amount)
+      data: response.nonConsolidatedValues.sort(comparePeriod).map(v => v.amount)
     }]
   }
 }
 
-const isInstansPeriod = (period: AccountPeriod): period is InstantPeriod => {
+const isInstantPeriod = (period: AccountPeriod): period is InstantPeriod => {
   const mayBeInstant = period as InstantPeriod;
   return mayBeInstant.instant?.length > 0 ?? false;
 }
-
+const comparePeriod = (a: TimeSeriesAnalysisValue, b: TimeSeriesAnalysisValue): number => {
+  if (isInstantPeriod(a.financialAccountPeriod)
+  && isInstantPeriod(b.financialAccountPeriod)) {
+    return a.financialAccountPeriod.instant
+      .localeCompare(b.financialAccountPeriod.instant);
+  }
+  else if (!isInstantPeriod(a.financialAccountPeriod)
+  && !isInstantPeriod(b.financialAccountPeriod)) {
+    return a.financialAccountPeriod.from
+      .localeCompare(b.financialAccountPeriod.from);
+  }
+  throw new Error('単位情報に不整合があります');
+}
 
 type AccountPeriod = InstantPeriod | DurationPeriod;
 type InstantPeriod = {
@@ -79,11 +92,32 @@ type TimeSeriesAnalysisValue = {
 };
 export type TimeSeriesAnalysisResposeValue = {
   accountName: string,
-  unit: unknown,
+  unit: Unit,
   corporation: { name: string },
   consolidatedValues: TimeSeriesAnalysisValue[],
   nonConsolidatedValues: TimeSeriesAnalysisValue[],
 }
+export type Unit = NormalUnit | DividedUnit
+type BaseUnit = { name: string }
+type NormalUnit = {
+  measure: string
+} & BaseUnit
+type DividedUnit = {
+  unitNumerator: string,
+  unitDenominator: string
+} & BaseUnit
+const isNormalUnit = (unit: Unit): unit is NormalUnit => {
+  const maybeNormalUnit = unit as DividedUnit;
+  return maybeNormalUnit.unitNumerator === null;
+};
+const createChartTitle = (data: TimeSeriesAnalysisResposeValue) => {
+  if (isNormalUnit(data.unit)) {
+    return `${data.accountName} ${data.unit.name}(${data.unit.measure})`
+  }
+
+  return `${data.accountName} ${data.unit.name}(${data.unit.unitNumerator} / ${data.unit.unitDenominator})`
+};
+
 export type LineChartData = 'waitingUserInput' | 'loading' | TimeSeriesAnalysisResposeValue;
 
 export const LineChart = (props: {
@@ -98,5 +132,16 @@ export const LineChart = (props: {
     && !data.nonConsolidatedValues.some(v => v)) {
     return <>データ無し</>;
   }
-  return <div role="main"><Line data={convertToChartData(data)} /></div>;
+  return <div role="main"><Line options={{
+    plugins: {
+      title: {
+        display: true,
+        text: createChartTitle(data),
+        padding: {
+          top: 10,
+          bottom: 20
+        }
+      }
+    }
+  }} data={convertToChartData(data)} /></div>;
 }
